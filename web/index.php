361fc96537f3c60d8db67cb341968d170389b25a
DanielMoran98 <33605732+DanielMoran98@users.noreply.github.com>
2017-11-12 19:58:05 +0000

    Added current files to repo

---
+++ web/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        'Index Page'
+    </title>
+    <style>@import 'web/css/style.css';</style>
+</head>
+
+<body>
+<div id="loginbar">
+    Username:
+    <input type="text" name="username">
+    Password:
+    <input type="password" name="password">
+    <input type="submit" value="Login">
+</div>
+<br/>
+<br/>
+<div id="container">
+
+
+    <header>
+        <img src = 'web/images/banner.jpg'>
+    </header>
+
+
+    <nav>
+        <ul>
+            <li class="navleft">Home</li>
+            <li>Characters</li>
+            <li>Gallery</li>
+            <li class="navright">Store</li>
+        </ul>
+    </nav>
+
+</div>
+    <footer>
+        <p>
+            Copyright 2017&copy;
+        </p>
+    </footer>
+
+</body>
+</html>
